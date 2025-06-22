182b3d43427df2f3d392458877b75813cda6cd00
Merai <intern.rj@merai.co>
2025-06-22 18:06:15 +0530

    fix: increase minimum length for description in disaster schema

--- src/dtos/disaster.dto.js
+++ src/dtos/disaster.dto.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const disasterSchema = z.object({
   title: z.string().min(5),
   location_name: z.string().min(5),
-  description: z.string().min(13),
+  description: z.string().min(15),
   tags: z.array(z.string()).optional(),
   owner_id: z.string().uuid(),
 });
