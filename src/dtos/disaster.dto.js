fb2bd04a08cd1e2733f236c4a3f9cdb8cb0c70b6
Rakesh Jain <101811197+Rakeshjaina777@users.noreply.github.com>
2025-06-22 17:50:36 +0530

    Update disaster.dto.js

--- src/dtos/disaster.dto.js
+++ src/dtos/disaster.dto.js
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const disasterSchema = z.object({
-  title: z.string().min(3),
-  location_name: z.string().min(3),
-  description: z.string().min(10),
+  title: z.string().min(4),
+  location_name: z.string().min(4),
+  description: z.string().min(12),
   tags: z.array(z.string()).optional(),
   owner_id: z.string().uuid(),
 });
